5380aa9d4d34ff902b4ad857a32f5704731666ef
Shyaboi <ian.shane.sears@gmail.com>
2021-03-12 00:07:47 -0700

    added sysout echo for each runner

--- noder.js
+++ noder.js
@@ -3,9 +3,10 @@ const exec = util.promisify(require('child_process').exec);
 
 
 
-async function lsExample() {
-  const { stdout, stderr } = await exec('python rrGetter.py');
+const noder = async(cmd)=> {
+  const { stdout, stderr } = await exec(cmd);
   console.log('stdout:', stdout);
   console.error('stderr:', stderr);
 }
-lsExample();
+noder('echo Noder! Ran!');
+noder('python rrGetter.py');
--- rustyRunner.rs
+++ rustyRunner.rs
@@ -1,19 +1,35 @@
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("java")
+    let jrunner = Command::new("java")
         .arg("JavaRunner")
         .output().unwrap_or_else(|e| {
             panic!("failed to execute process: {}", e)
     });
 
-    if output.status.success() {
-        let s = String::from_utf8_lossy(&output.stdout);
+    if jrunner.status.success() {
+        let s = String::from_utf8_lossy(&jrunner.stdout);
 
-        print!("rustc succeeded and stdout was:\n{}", s);
+        print!("RustyRunner Ran, and stdout was:\n{}", s);
     } else {
-        let s = String::from_utf8_lossy(&output.stderr);
+        let s = String::from_utf8_lossy(&jrunner.stderr);
 
-        print!("rustc failed and stderr was:\n{}", s);
+        print!("RustyRunner failed and stderr was:\n{}", s);
     }
+
+    let output = Command::new("echo")
+    .arg("Rusty! Runner! Ran! ")
+    .output().unwrap_or_else(|e| {
+        panic!("failed to execute process: {}", e)
+});
+
+if output.status.success() {
+    let s = String::from_utf8_lossy(&output.stdout);
+
+    print!("RustyRunner succeeded and stdout was:\n{}", s);
+} else {
+    let s = String::from_utf8_lossy(&output.stderr);
+
+    print!("RustyRunner failed and stderr was:\n{}", s);
+}
 }
