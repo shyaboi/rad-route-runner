7384d703fb31fa0d66bb5880bae1a39ae818b2ba
Shyaboi <ian.shane.sears@gmail.com>
2021-03-15 02:23:15 -0700

    added file upload and type parser, now launching from server

--- FlaskServer/Flask.py
+++ FlaskServer/Flask.py
@@ -1,48 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
 import os 
-from Routes import Home
-from Routes import File
+from Routes import Home,File
 
-# url = 'https://uselessapi.com/u-c-r/test'
-# url = 'https://uselessapi.com/u-c-r/rick'
-# url = 'https://uselessapi.com/u-c-r/toast'
-
-# print("Py rrGetter running!")
-
-# x = requests.get(url)
-
-# ae = x.headers
-
-# ct = ae.get('Content-Type')
-
-# dk= str(x.content)
-
-# cl= dk.replace("b'",'',1)
-# fcl= cl.replace("'",'',1)
-
-
-# if ct=="text/html; charset=utf-8":
-#     print('html or txt')
-#     os.system(f"python -m webbrowser -t {url}")
-#     pass
-
-# if ct=="application/json; charset=utf-8":
-#     print('json')
-#     pass
-
-# if d[0]=="<":
-#     pass
-# print("fin")
 
 app = Flask(__name__)
+
 api = Api(app)
 
-
-
-
-
-# api.add_resource(Home, '/')
+api.add_resource(Home, '/')
 
 api.add_resource(File, '/file')
 
--- FlaskServer/Routes/Home.py
+++ FlaskServer/Routes/Home.py
@@ -4,7 +4,7 @@ from flask_restful import Resource, Api
 class Home(Resource):
     
     def get(self):
-        os.system(f"cd ..")
-        os.system(f"bash Bashrunner.sh")
+     
+     
 
         return {'hello': 'world'}
