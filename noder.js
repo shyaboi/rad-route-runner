1b33be9721ced063353bfc740e2fdd88f6297f10
Shyaboi <ian.shane.sears@gmail.com>
2021-03-16 02:54:05 -0700

    getting data to noder in args

--- noder.js
+++ noder.js
@@ -2,10 +2,16 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 const fs = require ("fs")
 
-const writeData = () => {
-  const cb = ()=> {console.log('noder file writter ran')}
-  fs.appendFile('testFile.txt', '\nText Written From noder!\n', 'utf8', cb);
-}
+console.log(process.argv.pFile)
+
+console.log(typeof(process.argv))
+
+// let obj = process.argv[2]
+// console.log(obj)
+// const writeData = () => {
+//   const cb = ()=> {console.log('noder file writter ran')}
+//   fs.appendFile('testFile.txt', '\nText Written From noder!\n', 'utf8', cb);
+// }
 
 const noder = async (cmd) => {
   const { stdout, stderr } = await exec(cmd);
@@ -13,6 +19,6 @@ const noder = async (cmd) => {
   console.error("stderr:", stderr);
 };
 
-writeData()
+// writeData()
 noder("echo Noder! Ran!");
-noder("python pyRunner.py");
+// noder("python pyRunner.py");
--- pyRunner.py
+++ pyRunner.py
@@ -11,6 +11,12 @@ ext = data[0]['ext']
 
 if ext == 'py':
         print(data[0]['pFile'])
-        bro = compile(evalStatement, 'bra', 'exec')
+        bro = compile(evalStatement, 'evalCompile', 'exec')
         exec(bro)
+if ext == 'txt':
+        print(data[0]['pFile'])
+if ext == 'js':
+        pd = data[0]['pFile']
+        os.system(f"node noder.js {data}")
+
 # ok = json.loads(data)
