618ac4d2d181af45272b912093d6acc42abb47fc
Shyaboi <ian.shane.sears@gmail.com>
2021-03-15 17:19:45 -0700

    made route existance checker

--- FlaskServer/Flask.py
+++ FlaskServer/Flask.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restful import Resource, Api
 import os 
-from Routes import Home,SaveFile,GetFile
-
+from Routes import Home, SaveFile, GetFile, Exists
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 api = Api(app)
 
@@ -13,6 +15,10 @@ api.add_resource(Home, '/')
 api.add_resource(SaveFile, '/file')
 
 api.add_resource(GetFile, '/files/<string:route>')
+
+api.add_resource(Exists, '/exists/<string:route>')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- FlaskServer/Routes/SaveFile.py
+++ FlaskServer/Routes/SaveFile.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask import  request
 import shutil 
 from DBConn import mongo
+from .Exists import *
 
 class SaveFile(Resource):
     def post(self):
@@ -15,9 +16,10 @@ class SaveFile(Resource):
         parsedFile = f.read().decode('utf-8')
         fileExt = args.type
         route = args.route
+
         mongo.db.radRoute.insert({'route':route, 'ext':fileExt, 'pFile':parsedFile, 'fileKey':route+f.filename})
+        mongo.save_file(route+f.filename, f)
         # ff = f.save(f.filename)
         # shutil.move(f.filename, f"./uploads/{f.filename}")
-        mongo.save_file(route+f.filename, f)
 
         return "COol"
